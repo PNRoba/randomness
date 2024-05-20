549c86e02d3de5e5cd239bc4e20a3d448c962d3f
PNRoba <67026550+PNRoba@users.noreply.github.com>
2024-05-20 08:56:31 +0300

    Added comments diagrams.py

--- diagrams.py
+++ diagrams.py
@@ -1,3 +1,9 @@
+# Programme: Diagrams
+# Description:
+# From millions of float data points generates
+# diagrams (each containing 100 million data points)
+# that show the values in relation to the limits
+
 import os
 import matplotlib.pyplot as plt
 
@@ -5,6 +11,8 @@ def plot_delta_sequences_100(folder_path, name, hundredMil):
         all_delta_sequences = []
         filename = folder_path + "\delta_sequence-" + name + "-"
 
+        # extract and format data from delta sequence files
+        # up tp 100 million data points, to be visualised
         for i in range((hundredMil*100)-99, (hundredMil*100)+1):
             print("Currently computing: " + str(i) + " million")
             input_file_path = os.path.join(folder_path, filename + str(i) + "mil.txt")
@@ -12,6 +20,7 @@ def plot_delta_sequences_100(folder_path, name, hundredMil):
                 delta_sequence = [float(line.strip()) for line in file]
                 all_delta_sequences.extend(delta_sequence)
 
+        # Diagram settings
         plt.plot(all_delta_sequences, label=r'$\frac{S_{10}(n) - 4.5n}{\sqrt{2 \cdot 8.25n \log(\log(n))}}$')
         plt.axhline(y=1, color='red', linestyle='--', label='Upper Bound (+1)')
         plt.axhline(y=0, color='grey', linestyle='--', label='Middle Bound (0)')
@@ -24,6 +33,7 @@ def plot_delta_sequences_100(folder_path, name, hundredMil):
 
         plt.legend().set_visible(False)
 
+        # Save diagram
         output_file = os.path.join('path\Delta Sequences', f'delta_sequence_e_10mil.png')
         plt.savefig(output_file)
 
@@ -31,9 +41,17 @@ def plot_delta_sequences_100(folder_path, name, hundredMil):
 
 
 def main():
-    name = 'pi'
+    # Name of constant to generate the diagrams for
+    name = 'sqrt2'
+    # Name of folder (used for locating data for diagram generation, and for storing generated diagrams)
     output_file_path = 'path\Delta Sequences'
 
+    # Give:
+    # - Folder path (containing data + for storing diagrams)
+    # - Name of the constant (included in the pattern of delta sequence text file titles)
+    # - index describing which 100 million data to visualise 
+    #   (1 -> from 1 to 100 million, 2 -> from 101 to 200 million, etc.)
+    # For 1 billion - 10 diagrams, for 10 billion - 100 diagrams
     plot_delta_sequences_100(output_file_path, name, 1)
     # plot_delta_sequences_100(output_file_path, name, 2)
     # plot_delta_sequences_100(output_file_path, name, 3)
