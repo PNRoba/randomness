31a5c2e36e696e01a9d626a0990c8743764bfabd
PNRoba <67026550+PNRoba@users.noreply.github.com>
2024-05-20 09:19:01 +0300

    Add comments last51chars.py

--- last51chars.py
+++ last51chars.py
@@ -1,3 +1,10 @@
+# Programme: Last 51 characters
+# Description:
+# Prints out last 51 characters from a text file
+# that contains 1 billion characters
+# (can be modified to print last 51 characters
+# of a text file containing 10 billion)
+
 import os
 
 def print_last_chars_of_files(folder_path):
@@ -7,14 +14,16 @@ def print_last_chars_of_files(folder_path):
 
     file_list = os.listdir(folder_path)
 
+    # For each file print last 51 characters (last 51 digits of each constant)
     for file_name in file_list:
         if file_name.endswith('.txt'):
             file_path = os.path.join(folder_path, file_name)
             with open(file_path, 'r') as file:
-                file.seek(999999949)
+                file.seek(999999949) # Add 9 in the front to print last 51 chars of 10 billion long char text file
                 rest_of_file = file.read()
                 print(f"Last 51 characters of {file_name}: {rest_of_file[-51:]}")
 
+# Folder containing constant digit strings up to 1 billion length
 folder_path = 'konstantes\done'
 
 print_last_chars_of_files(folder_path)
