f71a9ef63d968b50d8fa5b3c0315594518e336a3
PNRoba <67026550+PNRoba@users.noreply.github.com>
2024-05-20 08:25:05 +0300

    Added comments counter-pi.py

--- counter-pi.py
+++ counter-pi.py
@@ -1,7 +1,16 @@
+# Programme: Counter Pi
+# Description:
+# Counts each decimal digit occurances in
+# the 10 billion pi decimal digit string.
+# (Pi digit string seperated in 10 chunks)
+
 def count_digits_after_dot(file_path, counts):
+    # Read file
     with open(file_path, 'r') as file:
         content = file.read()
 
+    # Iterate through the file content char by char
+    # Add +1 each time the respective digit occurs
     for char in content:
         if char.isdigit():
             counts[char] += 1
@@ -13,7 +22,7 @@ def count_digits_after_dot(file_path, counts):
     for digit, count in counts.items():
         print(f'Mentions of {digit} after \'.\': {count}')
 
-# Example usage
+# Folder of pi digit string chunks
 output_file_path = 'path\Pi-chunks'
 
 counts = {'0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
@@ -28,5 +37,3 @@ count_digits_after_dot(output_file_path+'\chunk_7.txt', counts)
 count_digits_after_dot(output_file_path+'\chunk_8.txt', counts)
 count_digits_after_dot(output_file_path+'\chunk_9.txt', counts)
 count_digits_after_dot(output_file_path+'\chunk_10.txt', counts)
-
-
